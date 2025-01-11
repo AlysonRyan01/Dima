c8b8fdd56b731a9091f88318be46d8afb3059528
Alyson Ryan Ullirsch <sdhuahdsaih@gmail.com>
2025-01-11 00:15:49 -0300

    Dima.Web

--- Dima.Api/obj/Debug/net9.0/Dima.Api.AssemblyInfo.cs
+++ Dima.Api/obj/Debug/net9.0/Dima.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Dima.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59a7c34c58f0024b74c68c50d838cceb6abc6183")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5936ca129d01d78c6ef8044d207e85acd1b69a97")]
 [assembly: System.Reflection.AssemblyProductAttribute("Dima.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Dima.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Dima.Core/obj/Debug/net9.0/Dima.Core.AssemblyInfo.cs
+++ Dima.Core/obj/Debug/net9.0/Dima.Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Dima.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59a7c34c58f0024b74c68c50d838cceb6abc6183")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5936ca129d01d78c6ef8044d207e85acd1b69a97")]
 [assembly: System.Reflection.AssemblyProductAttribute("Dima.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Dima.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
